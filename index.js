8bf426b050f7b3f45517ffe04fb4a0d931d8b4cb
Paulo César da Silva <paulocsmg@gmail.com>
2019-06-05 21:33:10 -0300

    Finalizado

--- index.js
+++ index.js
@@ -13,7 +13,15 @@ const config = require('./config');
 app.get('/conta/:id', (req, res) => {
   const id = req.params.id;
   console.log('GET CONTA', id);
-  res.redirect(`https://www.facebook.com/v3.3/dialog/oauth?client_id=${config.appId}&redirect_uri=http://localhost:3000/conta/cadastro/${id}&state=stateOauthAAS`)
+
+  // Esta aplicação não persiste o id do usuário
+  // Para fins didáticos verificamos apenas se o
+  // tamanho do id é maior do que 5.
+  if (id.length > 5) {
+    res.status(200).send({novo_id: id});
+  } else {
+    res.redirect(`https://www.facebook.com/v3.3/dialog/oauth?client_id=${config.appId}&redirect_uri=http://localhost:3000/conta/cadastro/${id}&state=stateOauthAAS`)
+  }
 });
 
 app.get('/conta/cadastro/:id', (req, res) => {
@@ -22,23 +30,19 @@ app.get('/conta/cadastro/:id', (req, res) => {
   const state = req.query.state;
   axios.get(`https://graph.facebook.com/v3.3/oauth/access_token?client_id=${config.appId}&redirect_uri=http://localhost:3000/conta/cadastro/${id}&state=${state}&client_secret=${config.appSecretKey}&code=${code}`)
     .then(({data}) => {
-      console.log('2.1 resp', data);
       const token = data.access_token;
       axios.get(`https://graph.facebook.com/debug_token?input_token=${token}&access_token=${config.accessToken}`)
         .then(({data}) => {
-          console.log('2.2 resp', data);
           const userId = data.data.user_id;
-          console.log('2.2 token', token);
-          axios.get(`https://graph.facebook.com/${userId}/?acess_token=${token}`)
-            .then((resp) => {
-              console.log('2.3 resp', resp);
+          axios.get(`https://graph.facebook.com/${userId}/?access_token=${token}`)
+            .then(({data}) => {
+              return res.redirect(`http://localhost:3000/conta/${data.id}`);
             })
             .catch((erro) => {
-              console.error('2.3 erro', erro);
+              return res.status(400).send('erro');
             });
         });
     });
-  res.sendStatus(200);
 });
 
 app.listen(3000, () => console.log('Aplicação pronta e usando a porta 3000.'));
